b699d661ae7abd3ecc00b953025d6761f32b27c1
Alex Bezuska <abezuska@gmail.com>
2015-10-23 13:25:16 -0400

    test file writing

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var fs = require('fs');
 
-fs.writeFile('message.txt', 'Hello Node.js', function (err) {
+fs.readFile('file1.json', function (err, data) {
   if (err) throw err;
-  console.log('It\'s saved!');
+  var file1 = JSON.parse(data);
+  file1.file2 = [
+    {"stuff":"things"}
+  ];
+  file1.file3 = [
+    {"stuff":"things"}
+  ];
+  writeOut(file1);
 });
+
+function writeOut(data){
+  fs.writeFile('newfile.json', JSON.stringify(data), function (err) {
+    if (err) throw err;
+    console.log(data);
+    console.log('It\'s saved!');
+  });
+}
